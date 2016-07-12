a0f7c48ad8b99714cba0d67890d19fddb5bbf528
Kelvin Chu <Kelvin@Kelvins-MacBook-Pro.local>
2016-07-12 02:39:04 -0400

    Add a constants class

---
+++ Source/Constants.hx
@@ -0,0 +1,19 @@
+package;
+
+class Constants
+{
+    public static inline var GRID_ELEMENT_WIDTH = 10;
+    public static inline var GRID_ELEMENT_HEIGHT = 10;
+
+    public static inline var GRID_ELEMENT_SPACING = 2;
+
+    public static inline var MIN_SNAKE_SECTIONS_COUNT = 5;
+
+    public static inline var SNAKE_STARTING_XPOS = 20;
+    public static inline var SNAKE_STARTING_YPOS  = 20;
+
+    public static inline var DIRECTION_RIGHT = "Right";
+    public static inline var DIRECTION_LEFT = "Left";
+    public static inline var DIRECTION_UP = "Up";
+    public static inline var DIRECTION_DOWN = "Down";
+}
