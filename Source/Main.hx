46d5d55e664b5dbbd5d1be9319c06c4bb54379aa
unknown <unknown@unknown.com>
2016-07-15 02:02:01 -0400

    Add game over and game reset
    Add simple game over that resets the game state and creates a new game

--- Source/Main.hx
+++ Source/Main.hx
@@ -11,9 +11,18 @@ import openfl.utils.Timer;
 import openfl.events.TimerEvent;
 import openfl.ui.Keyboard;
 
+enum GameState
+{
+	Paused;
+	GameOver;
+	Playing;
+}
+
 class Main extends Sprite
 {	
 	private var _isInitialzed:Bool;
+
+	private var _currentGameState:GameState;
 
 	private var _snake:Snake;
 	private var _food:Food;
@@ -23,50 +32,6 @@ class Main extends Sprite
 	private var _updateList:List<IUpdatable> = new List<IUpdatable>();
 	private var _inputProcessor:IInputProcessorAdapter;
 
-
-	/* ENTRY POINT */
-
-	function onResize(e)
-	{
-		if (!_isInitialzed)
-		{
-			init();
-		}
-		// else (resize or orientation change)
-	}
-
-	function init()
-	{
-		if (_isInitialzed)
-		{
-			return;
-		}
-		_isInitialzed = true;
-		_updateList.clear();
-
-		stage.addEventListener(Event.ENTER_FRAME, onUpdate);
-
-		_inputProcessor = new KeyboardInputProcessor();
-
-		// TODO: take a look at IOC via minject
-
-		_scoring = new Scoring();
-		this.addChild(_scoring);
-
-		_snake = new Snake(Constants.MIN_SNAKE_SECTIONS_COUNT);
-        this.addChild(_snake);
-
-		_food = new Food(0xFF0000, _snake.Model);
-		this.addChild(_food);
-
-		_snake.Food = _food;
-		_snake.SnakeDiedSignal.add(onSnakeDied);
-		
-		_inputProcessor.enable();
-		_updateList.add(_snake);
-	}
-
-	/* SETUP */
 
 	public function new()
 	{
@@ -84,6 +49,57 @@ class Main extends Sprite
 		//
 	}
 
+	private function onResize(e)
+	{
+		if (!_isInitialzed)
+		{
+			init();
+		}
+		// else (resize or orientation change)
+	}
+
+	private function init()
+	{
+		if (_isInitialzed)
+		{
+			return;
+		}
+		_isInitialzed = true;
+		_updateList.clear();
+
+		stage.addEventListener(Event.ENTER_FRAME, onUpdate);
+
+		_inputProcessor = new KeyboardInputProcessor();
+
+		_scoring = new Scoring();
+		this.addChild(_scoring);
+
+		createNewGame();
+		setGameState(GameState.Playing);
+	}
+
+	private function setGameState(gameState:GameState)
+	{
+		_currentGameState = gameState;
+		switch (_currentGameState)
+		{
+			case GameState.Paused:
+			{
+
+			}
+
+			case GameState.GameOver:
+			{
+				resetGame();
+			}
+
+			case GameState.Playing:
+			{
+
+			}
+		}
+	}
+
 	private function onUpdate(evt:Event)
 	{
 		for (obj in _updateList)
@@ -99,14 +115,15 @@ class Main extends Sprite
 	{
 		removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
 		stage.addEventListener(Event.RESIZE, onResize);
-		#if ios
-		haxe.Timer.delay(init, 100); // iOS 6
-		#else
 		init();
-		#end
 	}
 
 	private function onSnakeDied()
+	{
+		setGameState(GameState.GameOver);
+	}
+
+	private function resetGame()
 	{
 		// stop all input
 		if (_inputProcessor != null)
@@ -114,18 +131,45 @@ class Main extends Sprite
 			_inputProcessor.disable();
 		}
 
+		// clear update list
+		_updateList.clear();
+
+		// stop update
+		stage.removeEventListener(Event.ENTER_FRAME, onUpdate);
+
 		// remove snake
+		this.removeChild(_snake);
 
 		// generate food
+		this.removeChild(_food);
 
-		// start game
+		// start new game
+		createNewGame();	
+	}
+
+	private function createNewGame()
+	{
+		// reset score
+
+		// create new game elements
+		_snake = new Snake(Constants.MIN_SNAKE_SECTIONS_COUNT);
+        this.addChild(_snake);
+
+		_food = new Food(0xFF0000, _snake.Model);
+		this.addChild(_food);
+
+		_snake.Food = _food;
+		_snake.SnakeDiedSignal.add(onSnakeDied);
+		
+		_updateList.add(_snake);
 
 		// accept input
 		if (_inputProcessor != null)
 		{
 			_inputProcessor.enable();
 		}
-		
-		stage.removeEventListener(Event.ENTER_FRAME, onUpdate);
+
+		// start update
+		stage.addEventListener(Event.ENTER_FRAME, onUpdate);
 	}
 }
